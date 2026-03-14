f8610635a461368991f3e9ef53bb64a65a6898f1
Abhinav <abhinav7268@gmail.com>
2026-03-14 15:24:11 +0530

    setup local environment for interview feature development

--- ssb-backend/utils/db.js
+++ ssb-backend/utils/db.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000
